edc3c4ceef51d826e2a3548213e3090df8b7e456
Andrew Stacey <loopspace@mathforge.org>
2021-11-01 23:08:44 +0000

    Voronoi football diagrams
    Version of the Voronoi diagrams with a football pitch and eleven points.

---
+++ football/main.js
@@ -0,0 +1,511 @@
+var gl; // A global variable for the WebGL context
+
+var width;
+var height;
+var aspect;
+var pitchAspect;
+
+// Matrix stack
+var matrixStack = [];
+
+// Perspective matrix
+var perspectiveMatrix;
+
+// Current touched object
+var voronoiInTouch;
+
+// Parse query string
+var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+        var p=a[i].split('=', 2);
+        if (p.length == 1)
+            b[p[0]] = "";
+        else
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+})(window.location.search.substr(1).split('&'));
+
+function queryBoolean(key,def) {
+    var b = def;
+    if ("no" + key in qs) {
+	if (qs["no" + key].toLowerCase() != "false" && qs["no" + key] != "0") {
+	    b = false;
+	}
+    }
+    if (key in qs) {
+	if (qs[key].toLowerCase() == "false" || qs[key] == "0") {
+	    b = false;
+	}
+    }
+    return b;
+}
+
+// Based on http://stackoverflow.com/a/21059677
+
+function start() {
+    var canvas = document.getElementById('glCanvas');
+    var pitch = document.getElementById('pitch');
+
+    pitchAspect = pitch.width/pitch.height;
+    
+    width = window.innerWidth
+	|| document.documentElement.clientWidth
+	|| document.body.clientWidth;
+
+    height = window.innerHeight
+	|| document.documentElement.clientHeight
+	|| document.body.clientHeight;
+
+    width -= 25;
+    height -= 25;
+    width /= pitchAspect;
+    var size = Math.min(width,height);
+    width = pitchAspect * size;
+    height = size;
+
+    aspect = Math.min(width/2,height);// - 25;
+//    aspect *= .8;
+
+    canvas.width = width;
+    canvas.height = height;
+
+    // Initialize the GL context
+    gl = initWebGL(canvas);
+
+    // Only continue if WebGL is available and working
+    if (!gl) {
+	return;
+    }
+
+
+    // Set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Enable depth testing
+    gl.enable(gl.DEPTH_TEST);
+    // Near things obscure far things
+    gl.depthFunc(gl.LEQUAL);
+    // Clear the color as well as the depth buffer.
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    voronoi = new Voronoi();
+
+    drawScene();
+    canvas.addEventListener('wheel',doWheel);
+    canvas.addEventListener('mousedown',doMouseDown);
+    canvas.addEventListener('mousemove',doMouseMove);
+    canvas.addEventListener('mouseup',doMouseUp);
+
+    canvas.addEventListener('touchstart',doTouchStart);
+    canvas.addEventListener('touchmove',doTouchMove);
+    canvas.addEventListener('touchend',doTouchEnd);
+    
+    window.addEventListener('resize',resetSize);
+
+    /*
+    var chkbx = document.getElementById('npts');
+    chkbx.addEventListener('change', setVoronoiType);
+    chkbx.checked = queryBoolean("numpoints",false);
+    setVoronoiTypeAux(chkbx);
+
+    chkbx = document.getElementById('linear');
+    chkbx.addEventListener('change', setVoronoiDistance);
+    chkbx.checked = queryBoolean("linear",true);
+    
+    setVoronoiDistanceAux(chkbx);
+
+    var elt;
+    elt = document.getElementById('wgt');
+    elt.addEventListener('change', setVoronoiParams);
+    if (qs.weight) elt.value = qs.weight;
+    elt = document.getElementById('dis');
+    elt.addEventListener('change', setVoronoiParams);
+    if (qs.delay) elt.value = qs.delay;
+    elt = document.getElementById('qnt');
+    elt.addEventListener('change', setVoronoiParams);
+    if (qs.extent) elt.value = qs.extent;
+
+    elt = document.getElementById('wgts');
+    elt.addEventListener('change', setVoronoiParams);
+    elt.checked = queryBoolean("weights",false);
+    
+    elt = document.getElementById('dlys');
+    elt.addEventListener('change', setVoronoiParams);
+    elt.checked = queryBoolean("delays",false);
+
+    elt = document.getElementById('exts');
+    elt.addEventListener('change', setVoronoiParams);
+    elt.checked = queryBoolean("extents",false);
+
+    setVoronoiParams();
+//    elt = document.getElementById('qnt');
+//    c = elt.value;
+//    elt.addEventListener('change', setVoronoiParams);
+
+    elt = document.getElementById('regenerate');
+    elt.addEventListener('click', regenVoronoi);
+    */
+    //btn.addEventListener('click',showParameters);
+    //showParameters();
+    /*
+      Make the question mark toggle the help pane
+     */
+    var hlnk = document.getElementById('helplink');
+    var hdv = document.getElementById('help');
+    hlnk.addEventListener('click', function(e) {
+        e.preventDefault();
+        if (hdv.style.display == 'none' || hdv.style.display == '') {
+            hdv.style.display = 'block';
+        } else {
+            hdv.style.display = 'none';
+        }
+        return false;
+    });
+    /*
+      Set the help pane height to the window height,
+      Should probably update on resize
+     */
+    var h = window.innerHeight - 20;
+    hdv.style.height = h + 'px';
+
+//    document.getElementById("dl").addEventListener('click', dlCanvas, false);
+}
+
+/* From https://stackoverflow.com/a/12796748/315213 */
+/* REGISTER DOWNLOAD HANDLER */
+/* Only convert the canvas to Data URL when the user clicks. 
+   This saves RAM and CPU ressources in case this feature is not required. */
+function dlCanvas() {
+    var canvas = document.getElementById('glCanvas');
+    var dt = canvas.toDataURL('image/png');
+    /* Change MIME type to trick the browser to downlaod the file instead of displaying it */
+    dt = dt.replace(/^data:image\/[^;]*/, 'data:application/octet-stream');
+
+    /* In addition to <a>'s "download" attribute, you can define HTTP-style headers */
+    dt = dt.replace(/^data:application\/octet-stream/, 'data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png');
+
+    this.href = dt;
+};
+
+function resetSize() {
+    var hdv = document.getElementById('help');
+    /*
+      Set the help pane height to the window height,
+     */
+    var h = window.innerHeight - 20;
+    hdv.style.height = h + 'px';
+
+    
+    var canvas = document.getElementById('glCanvas');
+    var pitch = document.getElementById('pitch');
+
+    var pitchAspect = pitch.width/pitch.height;
+
+    width = window.innerWidth
+	|| document.documentElement.clientWidth
+	|| document.body.clientWidth;
+
+    height = window.innerHeight
+	|| document.documentElement.clientHeight
+	|| document.body.clientHeight;
+
+    width -= 25;
+    height -= 25;
+    width /= pitchAspect;
+    var size = Math.min(width,height);
+    width = size*pitchAspect;
+    height = size;
+
+    aspect = Math.min(width/2,height);// - 25;
+//    aspect *= .8;
+
+    canvas.width = width;
+    canvas.height = height;
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    drawScene();
+}
+
+function initWebGL(canvas) {
+    gl = null;
+  
+    // Try to grab the standard context. If it fails, fallback to experimental.
+    gl = canvas.getContext('webgl', {preserveDrawingBuffer: true}) || canvas.getContext('experimental-webgl', {preserveDrawingBuffer: true});
+  
+    // If we don't have a GL context, give up now
+    if (!gl) {
+	alert('Unable to initialize WebGL. Your browser may not support it.');
+    }
+  
+    return gl;
+}
+
+function setVoronoiType(e) {
+    setVoronoiTypeAux(e.target);
+}
+
+function setVoronoiTypeAux(e) {
+    voronoi.setType(e.checked);
+    toggleElements()
+    drawScene();
+}
+
+function setVoronoiDistance(e) {
+    setVoronoiDistanceAux(e.target);
+}
+
+function setVoronoiDistanceAux(e) {
+    voronoi.setLinear(e.checked);
+    toggleElements();
+    drawScene();
+}
+
+function toggleElements() {
+    var lin = document.getElementById('linear').checked;
+    var mul = document.getElementById('npts').checked;
+    var linelts = document.getElementsByClassName('linear');
+    var logelts = document.getElementsByClassName('log');
+    var mulelts = document.getElementsByClassName('multipleparam');
+    var sinelts = document.getElementsByClassName('singleparam');
+
+    var lon, loff, mon, moff, loffclass, moffclass;
+    if (lin) {
+	lon = linelts;
+	loff = logelts;
+	loffclass = 'log';
+    } else {
+	lon = logelts;
+	loff = linelts;
+	loffclass = 'linear';
+    }
+    if (mul) {
+	mon = mulelts;
+	moff = sinelts;
+	moffclass = 'singleparam';
+    } else {
+	mon = sinelts;
+	moff = mulelts;
+	moffclass = 'multipleparam';
+    }
+
+    for (var i = 0; i < loff.length; i++) {
+	loff[i].style.display = 'none';
+    }
+    for (var i = 0; i < moff.length; i++) {
+	moff[i].style.display = 'none';
+    }
+    for (var i = 0; i < lon.length; i++) {
+	if (!lon[i].classList.contains(moffclass)) {
+	    lon[i].style.display = 'table-row';
+	}
+    }
+    for (var i = 0; i < mon.length; i++) {
+	if (!mon[i].classList.contains(loffclass)) {
+	    mon[i].style.display = 'table-row';
+	}
+    }
+}
+
+function setVoronoiParams(e) {
+    var elt, a,b,c,d,e,f;
+    elt = document.getElementById('wgt');
+    a = elt.value;
+
+    elt = document.getElementById('dis');
+    b = elt.value;
+
+    elt = document.getElementById('qnt');
+    c = elt.value;
+    
+    elt = document.getElementById('wgts');
+    d = elt.checked;
+
+    elt = document.getElementById('dlys');
+    e = elt.checked;
+
+    elt = document.getElementById('exts');
+    f = elt.checked;
+
+    voronoi.setParams(a,b,c,d,e,f);
+    drawScene();
+}    
+
+function regenVoronoi(e) {
+    voronoi.regenerate();
+    drawScene();
+}
+
+function showParameters() {
+    showParametersAux(mandel);
+    showParametersAux(julia);
+}
+
+function showParametersAux(f) {
+    var vp,td;
+    vp = f.getViewport();
+    td = document.getElementById(f.name + '_ll');
+    td.innerHTML = '(' + vp[0] + ',' + vp[1] + ')';
+    td = document.getElementById(f.name + '_ur');
+    td.innerHTML = '(' + vp[2] + ',' + vp[3] + ')';
+    vp = f.getCentre();
+    td = document.getElementById(f.name + '_ce');
+    td.innerHTML = '(' + vp[0] + ',' + vp[1] + ')';
+}
+
+function drawScene() {
+//    julia.setParameter(mandel.getCentre());
+    
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    clearMatrices();
+
+    //    perspectiveMatrix = makePerspective(45, width/height, 0.1, 100.0);
+    perspectiveMatrix = makeOrtho(0,width,0,height,-1,100);
+
+    pushMatrix();
+    mvTranslate([width/2,height/2,0]);
+    //    mvScale([aspect * pitchAspect,aspect,1]);
+    mvScale([width/2,height/2,1]);
+    voronoi.draw();
+    popMatrix();
+}
+
+function multMatrix(m) {
+    matrixStack[matrixStack.length - 1] = matrixStack[matrixStack.length - 1].x(m);
+}
+
+function mvTranslate(v) {
+    multMatrix(Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4());
+}
+
+function mvScale(v) {
+    multMatrix(Matrix.Scale($V([v[0], v[1], v[2]])).ensure4x4());
+}
+
+function setMatrixUniforms(s) {
+    var pUniform = gl.getUniformLocation(s, "uPMatrix");
+    gl.uniformMatrix4fv(pUniform, false, new Float32Array(perspectiveMatrix.flatten()));
+
+    var mvUniform = gl.getUniformLocation(s, "uMVMatrix");
+    gl.uniformMatrix4fv(mvUniform, false, new Float32Array(currentMatrix().flatten()));
+}
+
+function doWheel(e) {
+}
+
+function doMouseDown(e) {
+    voronoiInTouch = null;
+    if (voronoi.isTouchedBy(e)) {
+	voronoi.doMouseDown(e);
+	voronoiInTouch = true;
+	drawScene();
+    }
+}
+
+function doMouseMove(e) {
+    if (voronoiInTouch) {
+	voronoi.doMouseMove(e);
+	drawScene();
+    }
+}
+
+function doMouseUp(e) {
+    if (voronoiInTouch) {
+	voronoi.doMouseUp(e);
+	drawScene();
+    }
+}
+
+var ongoingTouches = [];
+
+function doTouchStart(e) {
+    e.preventDefault();
+    var update = false;
+    var touches = e.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+	if (voronoi.isTouchedBy(touches[i])) {
+	    ongoingTouches.push([voronoi,copyTouch(touches[i])]);
+	    voronoi.doTouchStart(touches[i]);
+	    update = true;
+	}
+    }
+    if (update) {
+	drawScene();
+    }
+}
+
+function doTouchMove(e) {
+    e.preventDefault();
+    var update = false;
+    var touches = e.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+	var idx = ongoingTouchIndexById(touches[i].identifier);
+	if (idx >= 0) {
+	    ongoingTouches[idx][1] = copyTouch(touches[i]);
+	    ongoingTouches[idx][0].doTouchMove(touches[i]);
+	    update = true;
+	}
+    }
+    if (update) {
+	drawScene();
+    }
+}
+
+function doTouchEnd(e) {
+    e.preventDefault();
+    var update = false;
+    var touches = e.changedTouches;
+
+    for (var i = 0; i < touches.length; i++) {
+	var idx = ongoingTouchIndexById(touches[i].identifier);
+
+	if (idx >= 0) {
+	    ongoingTouches[idx][0].doTouchEnd(touches[i]);
+	    ongoingTouches.splice(idx,1);
+	    update = true;
+	}
+    }
+    if (update) {
+	drawScene();
+    }
+}
+
+function pushMatrix() {
+    var m = matrixStack[matrixStack.length - 1].dup();
+    matrixStack.push(m);
+}
+
+function popMatrix() {
+    if (matrixStack.length > 1)
+	matrixStack.pop();
+}
+
+function resetMatrix() {
+    matrixStack[matrixStack.length - 1] = Matrix.I(4);
+}
+
+function currentMatrix() {
+    return matrixStack[matrixStack.length - 1];
+}
+
+function clearMatrices() {
+    matrixStack = [Matrix.I(4)];
+}
+
+function copyTouch(touch) {
+    return {id: touch.identifier, pageX: touch.pageX, pageY: touch.pageY, target: touch.target}
+}
+
+function ongoingTouchIndexById(idToFind) {
+  for (var i = 0; i < ongoingTouches.length; i++) {
+    var id = ongoingTouches[i][1].id;
+    
+    if (id == idToFind) {
+      return i;
+    }
+  }
+  return -1;    // not found
+}
---
+++ football/voronoi.js
@@ -0,0 +1,401 @@
+
+// Voronoi class
+function Voronoi() {
+    this.coordinates = new Float32Array([
+	1.0, 1.0,
+	0.0, 1.0,
+	1.0, 0.0,
+	0.0, 0.0
+    ]);
+    this.points = new Float32Array([
+	0.125,0.125,
+	0.125,0.375,
+	0.125,0.625,
+	0.125,0.875,
+	0.375,0.125,
+	0.375,0.375,
+	0.375,0.625,
+	0.375,0.875,
+	0.625,0.125,
+	0.625,0.375,
+	0.625,0.625,
+    ]);
+    var params = [];
+    for (var i = 0; i < 16; i++) {
+	params.push(1+Math.random())
+	params.push(Math.random()*.25)
+	params.push(1+Math.random())
+    }
+    this.params = new Float32Array(params);
+    this.numpoints = 11;
+    this.useWeights = false;
+    this.useDelays = false;
+    this.useExtents = false;
+    this.linear = true;
+    this.image = document.getElementById('pitch');
+    this.aspect = this.image.width / this.image.height;
+    this.initShaders();
+    this.initBuffers();
+}
+
+Voronoi.prototype.initShaders = function() {
+    var fragmentShader = getShader(gl,'shader-fs');
+    var vertexShader = getShader(gl, 'shader-vs');
+  
+    // Create the shader program
+  
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+  
+    // If creating the shader program failed, alert
+  
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+	console.log('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+    }
+  
+    gl.useProgram(shaderProgram);
+
+    this.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
+    gl.enableVertexAttribArray(this.vertexPositionAttribute);
+    
+    this.vertexCoordinateAttribute = gl.getAttribLocation(shaderProgram, 'aVertexCoordinate');
+    gl.enableVertexAttribArray(this.vertexCoordinateAttribute);
+
+    this.shaderProgram = shaderProgram;
+}
+
+Voronoi.prototype.initBuffers = function() {
+    this.vertices = new Float32Array([
+	1.0,  1.0,  0.0, // top right on screen
+	-1.0, 1.0,  0.0, // top left on screen
+	1.0,  -1.0, 0.0, // bottom right on screen
+	-1.0, -1.0, 0.0  // bottom left on screen
+    ]);
+
+    gl.useProgram(this.shaderProgram);
+    
+    this.squareVerticesBuffer = gl.createBuffer();
+    this.squareVerticesCoordinateBuffer = gl.createBuffer();
+
+    this.texture = gl.createTexture();
+
+    this.vertexCoordinateAttribute = gl.getAttribLocation(this.shaderProgram, 'aVertexCoordinate');
+
+//    this.parameterUniform = gl.getUniformLocation(this.shaderProgram,'params');
+    this.pointsUniform = gl.getUniformLocation(this.shaderProgram,'pts');
+    this.paramsUniform = gl.getUniformLocation(this.shaderProgram,'params');
+    this.numPointsUniform = gl.getUniformLocation(this.shaderProgram,'np');
+    this.useWeightsUniform = gl.getUniformLocation(this.shaderProgram,'wgts');
+    this.useDelaysUniform = gl.getUniformLocation(this.shaderProgram,'dlys');
+    this.useExtentsUniform = gl.getUniformLocation(this.shaderProgram,'exts');
+    this.isLinearUniform = gl.getUniformLocation(this.shaderProgram,'lin');
+    this.aspectUniform = gl.getUniformLocation(this.shaderProgram,'aspect');
+}
+
+Voronoi.prototype.doBindings = function() {
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.squareVerticesBuffer);
+    gl.vertexAttribPointer(this.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+    gl.bufferData(gl.ARRAY_BUFFER, this.vertices, gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.squareVerticesCoordinateBuffer);
+    gl.vertexAttribPointer(this.vertexCoordinateAttribute, 2, gl.FLOAT, false, 0, 0);
+    gl.bufferData(gl.ARRAY_BUFFER, this.coordinates, gl.DYNAMIC_DRAW);
+    
+    //    gl.uniform2fv(this.parameterUniform, this.parameters);
+    gl.uniform2fv(this.pointsUniform, this.points);
+    gl.uniform3fv(this.paramsUniform, this.params);
+    gl.uniform1f(this.aspectUniform, this.aspect);
+    if (this.useWeights) {
+	gl.uniform1f(this.useWeightsUniform, 1);
+    } else {
+	gl.uniform1f(this.useWeightsUniform, 0);
+    }
+    if (this.useDelays) {
+	gl.uniform1f(this.useDelaysUniform, 1);
+    } else {
+	gl.uniform1f(this.useDelaysUniform, 0);
+    }
+    if (this.useExtents) {
+	gl.uniform1f(this.useExtentsUniform, 1);
+    } else {
+	gl.uniform1f(this.useExtentsUniform, 0);
+    }
+
+    if (this.linear) {
+	gl.uniform1f(this.isLinearUniform, 1);
+    } else {
+	gl.uniform1f(this.isLinearUniform, 0);
+    }
+    gl.uniform1f(this.numPointsUniform, this.numpoints);
+
+    this.bindTexture();
+}
+
+Voronoi.prototype.bindTexture = function() {
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, this.texture);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    // gl.NEAREST is also allowed, instead of gl.LINEAR, as neither mipmap.
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    // Prevents s-coordinate wrapping (repeating).
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    // Prevents t-coordinate wrapping (repeating).
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.uniform1i(gl.getUniformLocation(this.shaderProgram, 'pitch'), 0);
+//    gl.bindTexture(gl.TEXTURE_2D, null);
+
+}
+
+Voronoi.prototype.enableProgram = function() {
+    gl.useProgram(this.shaderProgram);
+
+    gl.enableVertexAttribArray(this.vertexPositionAttribute);    
+    gl.enableVertexAttribArray(this.vertexCoordinateAttribute);
+    
+}
+
+Voronoi.prototype.draw = function(m) {
+    this.enableProgram();
+    this.doBindings();
+
+    setMatrixUniforms(this.shaderProgram);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+    this.mvpMatrix = perspectiveMatrix.x(currentMatrix());
+}
+
+Voronoi.prototype.setType = function(b) {
+    if (b) {
+	this.numpoints = 16;
+    } else {
+	this.numpoints = 2;
+    }
+}
+
+Voronoi.prototype.setLinear = function(b) {
+    this.linear = b;
+}
+
+Voronoi.prototype.setParams = function(a,b,c,d,e,f) {
+    a = parseFloat(a);
+    if (isNaN(a)) {
+	a = 1;
+    }
+    b = parseFloat(b);
+    if (isNaN(b)) {
+	b = 0;
+    }
+    c = parseFloat(c);
+    if (isNaN(c)) {
+	c = 1;
+    }
+
+    this.dblparams[3] = a;
+    this.dblparams[4] = b;
+    this.dblparams[5] = c;
+    this.useWeights = d;
+    this.useDelays = e;
+    this.useExtents = f;
+}
+
+Voronoi.prototype.regenerate = function() {
+    var params = [];
+    for (var i = 0; i < 16; i++) {
+	params.push(1+Math.random())
+	params.push(Math.random()*.25)
+	params.push(1+Math.random())
+    }
+    this.params = new Float32Array(params);
+}
+
+Voronoi.prototype.setParameter = function(c) {
+    this.parameters = new Float32Array(c);
+}
+
+Voronoi.prototype.isTouchedBy = function(e) {
+    var pt = convertPoint(e,this.mvpMatrix);
+    if (pt.x < 1 && pt.x > 0 && pt.y < 1 && pt.y > 0)
+	return true;
+    return false;
+}
+
+Voronoi.prototype.doWheel = function(e) {
+    var pt = convertPoint(e,this.mvpMatrix);
+    var s = Math.pow(1.001,e.deltaY);
+}
+
+Voronoi.prototype.doMouseDown = function(e) {
+    if (e.button != 0)
+	return;
+    this.mouseIsMoving = false;
+    this.mousept = convertPoint(e,this.mvpMatrix);
+    var x,y,d,dd,p;
+    d = 4;
+    p = 0;
+    var pts;
+    if (this.numpoints == 2) {
+	pts = this.dblpoints;
+    } else {
+	pts = this.points;
+    }
+    for (var i = 0; i < this.numpoints; i++) {
+	x = pts[2*i];
+	y = pts[2*i+1];
+	dd = Math.pow(this.mousept.x - x,2) + Math.pow(this.mousept.y - y,2);
+	if (dd < d) {
+	    d = dd;
+	    p = 2*i;
+	}
+    }
+    this.touchpt = p;
+    this.touchoffset = [this.mousept.x - pts[p],this.mousept.y - pts[p+1]];
+    this.touchpts = pts;
+}
+
+Voronoi.prototype.doMouseMove = function(e) {
+    if (e.buttons != 1)
+	return;
+    this.mouseIsMoving = true;
+    
+    var pt = convertPoint(e,this.mvpMatrix);
+    this.touchpts[this.touchpt] = pt.x - this.touchoffset[0];
+    this.touchpts[this.touchpt+1] = pt.y - this.touchoffset[1];
+    this.draw();
+}
+
+Voronoi.prototype.doMouseUp = function(e) {
+    if (e.button != 0)
+	return;
+    this.mouseIsMoving = false;
+}
+
+/*
+  Needs implementing for touch screens
+*/
+Voronoi.prototype.doTouchStart = function(e) {
+    this.touchIsMoving = false;
+    this.touchpt = convertPoint(e,this.mvpMatrix);
+}
+
+Voronoi.prototype.doTouchMove = function(e) {
+    this.touchIsMoving = true;
+    
+    var pt = this.convertToViewport(e);
+    var dx = pt.x - this.touchpt.x;
+    var dy = pt.y - this.touchpt.y;
+    
+    // Adjust the viewport
+    this.viewport[0] -= dx;
+    this.viewport[1] -= dy;
+    this.viewport[2] -= dx;
+    this.viewport[3] -= dy;
+    this.setCoordinates();
+}
+
+Voronoi.prototype.doTouchEnd = function(e) {
+    if (!this.touchIsMoving) {
+	this.viewport = this.defaultViewport.slice();
+	this.setCoordinates();
+    }
+    this.touchIsMoving = false;
+}
+
+// Auxiliary functions
+
+function getShader(gl, ids, type) {
+    var shaderScript, theSource, currentChild, shader;
+
+    theSource = '';
+
+    if (typeof ids === "string")
+	ids = [ids];
+
+    for (var i = 0; i < ids.length; i++) {
+	shaderScript = document.getElementById(ids[i]);
+  
+	if (!shaderScript) {
+	    return null;
+	}
+  
+	theSource += shaderScript.text;
+    }
+
+    if (!type) {
+	if (shaderScript.type == 'x-shader/x-fragment') {
+	    type = gl.FRAGMENT_SHADER;
+	} else if (shaderScript.type == 'x-shader/x-vertex') {
+	    type = gl.VERTEX_SHADER;
+	} else {
+	    // Unknown shader type
+	    return null;
+	}
+    }
+    shader = gl.createShader(type);
+    gl.shaderSource(shader, theSource);
+    
+    // Compile the shader program
+    gl.compileShader(shader);  
+    
+    // See if it compiled successfully
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {  
+	console.log('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));  
+	gl.deleteShader(shader);
+	return null;  
+    }
+    
+    return shader;
+}
+
+var corners = [
+    Vector.create([1.0,  1.0,  0.0, 1.0]),
+    Vector.create([-1.0, 1.0,  0.0, 1.0]),
+    Vector.create([1.0,  -1.0, 0.0, 1.0]),
+    Vector.create([-1.0, -1.0, 0.0, 1.0])
+];
+
+function convertPoint(e,m) {
+    var v;
+    var minx = 0;
+    var maxx = 0;
+    var miny = 0;
+    var maxy = 0;
+    for (var i = 0; i < corners.length; i++) {
+	v = m.x(corners[i]);
+	minx = Math.min(minx,v.e(1)/v.e(4));
+	maxx = Math.max(maxx,v.e(1)/v.e(4));
+	miny = Math.min(miny,v.e(2)/v.e(4));
+	maxy = Math.max(maxy,v.e(2)/v.e(4));
+    }
+    var x,y;
+    
+    // Convert mouse point to normalised screen coordinates
+    if (e.offsetX !== undefined && e.offsetY !== undefined) {
+	x = e.offsetX;
+	y = e.offsetY;
+    } else if (e.layerX !== undefined && e.layerY !== undefined) {
+        x = e.layerX;
+	y = e.layerY;
+    } else {
+	x = e.pageX - e.target.offsetLeft;     
+	y = e.pageY - e.target.offsetTop;
+    };
+    x *= 2/width;
+    x -= 1;
+    y *= 2/height;
+    y -= 1;
+
+    // Convert to proportions along the sides of the square
+    x -= minx;
+    x /= (maxx - minx);
+    y -= miny;
+    y /= (maxy - miny);
+    // take account of the inversion in the y-coordinate
+    y = 1 - y;
+
+    return {x: x, y: y};
+}
